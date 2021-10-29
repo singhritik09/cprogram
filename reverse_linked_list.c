66c921d09b3fc6d69baf795ebe60d722a24540c7
spacewar <singhritik2711@gmail.com>
2021-10-29 19:55:36 +0530

    Third commit

---
+++ reverse_linked_list.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head;
+
+void nodeList(int n)
+{
+    struct node *newnode;
+    struct node *temp;
+    int userinput;
+    int i;
+    head=NULL;
+    printf("Enter Number of nodes: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        newnode=malloc(sizeof(struct node));
+        printf("Enter data for node %d: ",i);
+        scanf("%d",&userinput);
+        if(head==NULL)
+        {
+            newnode->data=userinput;
+            head=newnode;
+            temp=head;
+        }
+        else
+        {
+            newnode->data=userinput;
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    temp->next=NULL;
+}
+
+void display(struct node *head)
+{
+    struct node *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+}
+
+void reverse()
+{
+    struct node *prevptr=NULL;
+    struct node *currptr=head;
+    struct node *nextptr;
+    while(currptr!=NULL)
+    {
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+
+        prevptr=currptr;
+        currptr=nextptr;
+    }
+    head=prevptr;
+}
+
+int main()
+{
+    head=malloc(sizeof(struct node));
+    int n,k;
+    nodeList(n);
+    display(head);
+    printf("\n");
+    reverse();
+    display(head);
+    return 0;
+}
