2e12fe85f112dedda9ee565bb144039eacedcb65
spacewar <singhritik2711@gmail.com>
2021-10-30 01:48:53 +0530

    First commit

---
+++ sort.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head;
+
+void nodeList()
+{
+    struct node *newnode;
+    struct node *temp;
+    int userinput;
+    int i,n;
+    head=NULL;
+    printf("Enter Number of nodes: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        newnode=malloc(sizeof(struct node));
+        printf("Enter data for node %d: ",i);
+        scanf("%d",&userinput);
+        if(head==NULL)
+        {
+            newnode->data=userinput;
+            head=newnode;
+            temp=head;
+        }
+        else
+        {
+            newnode->data=userinput;
+            temp->next=newnode;
+            temp=newnode;
+        }
+    }
+    temp->next=NULL;
+}
+
+void display(struct node *head)
+{
+    struct node *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL");
+}
+
+void reverse()
+{
+    struct node *prevptr=NULL;
+    struct node *currptr=head;
+    struct node *nextptr;
+    while(currptr!=NULL)
+    {
+        nextptr=currptr->next;
+        currptr->next=prevptr;
+
+        prevptr=currptr;
+        currptr=nextptr;
+    }
+    head=prevptr;
+}
+
+struct node * sort(int n)
+{
+    struct node *temp;
+    int nodeCopy;
+    int i,j;
+    for(i=n;i>0;i--)
+    {
+        temp=head;
+        for(j=0;j<i;j++)
+        {
+            if(temp==NULL || temp->next==NULL)
+            {
+                break;
+            }
+            if(temp->data>temp->next->data)
+            {
+                nodeCopy=temp->data;
+                temp->data=temp->next->data;
+                temp->next->data=nodeCopy;
+            }
+
+            temp=temp->next;
+        }
+    }
+    return head;
+}
+
+int main()
+{
+    struct node *newhead;
+    int k;
+    nodeList();
+    display(head);
+    printf("\n");
+    newhead=sort(5);
+    display(newhead);
+    return 0;
+}
